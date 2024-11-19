cf657e1af7f953dfc92c958ee855c6fd93723720
BRANDOM20971 <nelsonacuna2007@cesanrafael.org>
2024-11-19 14:23:00 -0400

    botoneees

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,35 @@
+function sumar (){
+    let numerouno = 4;
+    let numerodos = 5;
+    let numerotres = 6;
+    let resultado = numerouno+numerodos+numerotres
+ alert("la suma es"+ resultado)
+}
+
+function restar (){
+    let numerouno = 4;
+    let numerodos = 5;
+    let numerotres = 6;
+    let resultado = numerouno-numerodos-numerotres
+ alert("la resta es"+ resultado)
+}
+
+function multiplicar (){
+    let numerouno = 4;
+    let numerodos = 5;
+    let numerotres = 6;
+    let resultado = numerouno*numerodos*numerotres
+ alert("la suma es"+ resultado)
+}
+
+function dividir (){
+    let numerouno = 4;
+    let numerodos = 5;
+    let numerotres = 6;
+    let resultado = numerouno/numerodos/numerotres
+ alert("la suma es"+ resultado)
+}
+
+
+
+
